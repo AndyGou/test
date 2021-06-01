2ca52b00d66a237b6863615dc03001a997ea8619
luffy <1179095884@qq.com>
2021-06-01 19:41:07 +0800

    update

--- src/main/java/com/system/entity/SCMDataVO.java
+++ src/main/java/com/system/entity/SCMDataVO.java
@@ -1,16 +1,9 @@
 package com.system.entity;
 
-import com.alibaba.excel.annotation.ExcelIgnore;
-import com.alibaba.excel.annotation.ExcelProperty;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
 import com.system.util.DateUtil;
 import org.springframework.beans.BeanUtils;
-
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
