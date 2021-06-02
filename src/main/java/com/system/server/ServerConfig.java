67112bfe65f1f13d7b7e05bf061164cc3f1fc80b
luffy <1179095884@qq.com>
2021-06-02 19:51:25 +0800

    update

--- src/main/java/com/system/server/ServerConfig.java
+++ src/main/java/com/system/server/ServerConfig.java
@@ -20,27 +20,35 @@ public class ServerConfig extends Thread {
 
     private final SCMService service = SpringUtil.getBean(SCMService.class);
 
-    private void handle(InputStream inputStream) throws IOException {
+    private static final ThreadLocal<Boolean> flag = new ThreadLocal<>();
 
-        byte[] bytes = new byte[1024 * 10];
-        int len = inputStream.read(bytes);
-        if (len != -1) {
-            String receive = new String(bytes, 0, len, StandardCharsets.UTF_8);
-            System.out.println("receive data: " + receive + " , current thread: " + Thread.currentThread().getName());
-            JSONObject jsonObject;
-            try {
-                jsonObject = JSON.parseObject(receive);
-            } catch (Exception e) {
-                jsonObject = null;
+    private void handle(InputStream inputStream) throws IOException {
+        try {
+            byte[] bytes = new byte[1024 * 10];
+            int len = inputStream.read(bytes);
+            if (len != -1) {
+                String receive = new String(bytes, 0, len, StandardCharsets.UTF_8);
+                System.out.println("receive data: " + receive + " , current thread: " + Thread.currentThread().getName());
+                JSONObject jsonObject;
+                try {
+                    jsonObject = JSON.parseObject(receive);
+                } catch (Exception e) {
+                    jsonObject = null;
+                }
+                if (jsonObject != null) {
+                    SCMData data = new SCMData();
+                    data.setTemperature(jsonObject.getBigDecimal(RequestKey.TEMP));
+                    data.setConcentration(jsonObject.getBigDecimal(RequestKey.AIR));
+                    data.setIntensity(jsonObject.getBigDecimal(RequestKey.ILLUMINATION));
+                    service.saveSCMData(data);
+                }
             }
-            if (jsonObject != null) {
-                SCMData data = new SCMData();
-                data.setTemperature(jsonObject.getBigDecimal(RequestKey.TEMP));
-                data.setConcentration(jsonObject.getBigDecimal(RequestKey.AIR));
-                data.setIntensity(jsonObject.getBigDecimal(RequestKey.ILLUMINATION));
-                service.saveSCMData(data);
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            flag.set(false);
         }
+
 
     }
 
@@ -49,7 +57,8 @@ public class ServerConfig extends Thread {
         try {
             System.out.println("client - " + socket.getRemoteSocketAddress() + " -> connect success");
             InputStream inputStream = socket.getInputStream();
-            while (true) {
+            flag.set(true);
+            while (flag.get()) {
                 handle(inputStream);
             }
         } catch (IOException socketException) {
